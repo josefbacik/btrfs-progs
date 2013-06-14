0a4d118b36a767aa6ebfc5a510048789ec533e57
Josef Bacik <jbacik@fusionio.com>
2013-06-14 14:24:12 -0400

    Btrfs-progs: commit the csum_root if we do --init-csum-tree
    This is just an oddity with the commit stuff in btrfs-progs.  It will just
    update the generation of the root you call with, which in btrfsck case would
    have been the fs_root.  But because we didn't actually update the fs_root we
    wouldn't have cow'ed the fs root and therefore the generation will not match the
    node which will make the file system unmountable.  Fix this by calling with the
    csum_root which is the one we're messing with.  Thanks,
    
    Signed-off-by: Josef Bacik <jbacik@fusionio.com>

--- cmds-check.c
+++ cmds-check.c
@@ -5227,7 +5227,7 @@ int cmd_check(int argc, char **argv)
 			return -EIO;
 		}
 
-		ret = btrfs_commit_transaction(trans, root);
+		ret = btrfs_commit_transaction(trans, info->csum_root);
 		if (ret)
 			exit(1);
 		goto out;
