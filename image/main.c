1977dfff88ef3b58598ec49e3517c1df0d26c0de
Josef Bacik <josef@toxicpanda.com>
2021-08-23 17:40:16 -0400

    btrfs-progs: image: keep track of seen blocks when walking trees
    Extent tree v2 no longer tracks all allocated blocks on the file system,
    so we'll have to default to walking trees to generate metadata images.
    There's an annoying drawback with walking trees with btrfs-image where
    we'll happily copy multiple blocks over and over again if there are
    snapshots.  Fix this by keeping track of blocks we've seen and simply
    skipping blocks that we've already queued up for copying.
    
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>

--- image/main.c
+++ image/main.c
@@ -70,6 +70,8 @@ struct metadump_struct {
 	pthread_cond_t cond;
 	struct rb_root name_tree;
 
+	struct extent_io_tree seen;
+
 	struct list_head list;
 	struct list_head ordered;
 	size_t num_items;
@@ -438,6 +440,7 @@ static void metadump_destroy(struct metadump_struct *md, int num_threads)
 		free(name->sub);
 		free(name);
 	}
+	extent_io_tree_cleanup(&md->seen);
 }
 
 static int metadump_init(struct metadump_struct *md, struct btrfs_root *root,
@@ -449,6 +452,7 @@ static int metadump_init(struct metadump_struct *md, struct btrfs_root *root,
 	memset(md, 0, sizeof(*md));
 	INIT_LIST_HEAD(&md->list);
 	INIT_LIST_HEAD(&md->ordered);
+	extent_io_tree_init(&md->seen);
 	md->root = root;
 	md->out = out;
 	md->pending_start = (u64)-1;
@@ -743,6 +747,14 @@ static int copy_tree_blocks(struct btrfs_root *root, struct extent_buffer *eb,
 	int nritems = 0;
 	int i = 0;
 	int ret;
+
+	bytenr = btrfs_header_bytenr(eb);
+	if (test_range_bit(&metadump->seen, bytenr,
+			   bytenr + fs_info->nodesize - 1, EXTENT_DIRTY, 1))
+		return 0;
+
+	set_extent_dirty(&metadump->seen, bytenr,
+			 bytenr + fs_info->nodesize - 1);
 
 	ret = add_extent(btrfs_header_bytenr(eb), fs_info->nodesize,
 			 metadump, 0);
