674cfc2758de2d799fdf7dd61cd6cb1f8481faab
Josef Bacik <josef@toxicpanda.com>
2021-08-31 20:55:22 -0400

    btrfs-progs: check: skip owner ref check for extent tree v2
    We do not track extent references for metadata in extent tree v2, simply
    skip this check if it is enabled.
    
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>

--- check/main.c
+++ check/main.c
@@ -4088,6 +4088,13 @@ static int check_owner_ref(struct btrfs_root *root,
 	int found = 0;
 	int ret;
 
+	/*
+	 * We don't have extent references for metadata with extent tree v2,
+	 * just return.
+	 */
+	if (btrfs_fs_incompat(gfs_info, EXTENT_TREE_V2))
+		return 0;
+
 	rbtree_postorder_for_each_entry_safe(node, tmp,
 					     &rec->backref_tree, node) {
 		if (node->is_data)
