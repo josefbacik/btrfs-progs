c04c6dd0e418ddf18e4a306fd71103d228b79a59
Josef Bacik <josef@toxicpanda.com>
2022-02-27 12:18:30 -0500

    btrfs-progs: add new COW rules for extent tree v2
    All of the previous COW rules will apply, but with extent tree v2 we add
    a new COW rule where we must COW if the snapshot id of the buffer is
    less than the current snapshot id of the root.  If the root's snapshot
    id has been increased we know that the block is now shared by a new root
    and we must cow this block.
    
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>

--- kernel-shared/ctree.c
+++ kernel-shared/ctree.c
@@ -635,6 +635,17 @@ static noinline int __btrfs_cow_block(struct btrfs_trans_handle *trans,
 	return 0;
 }
 
+static inline int should_cow_block_v2(struct btrfs_root *root,
+				      struct extent_buffer *buf)
+{
+	if (!btrfs_header_flag(buf, BTRFS_HEADER_FLAG_V2))
+		return 0;
+	if (btrfs_root_snapshot_id(&root->root_item) >
+	    btrfs_header_snapshot_id(buf))
+		return 1;
+	return 0;
+}
+
 static inline int should_cow_block(struct btrfs_trans_handle *trans,
 				   struct btrfs_root *root,
 				   struct extent_buffer *buf)
@@ -660,7 +671,8 @@ static inline int should_cow_block(struct btrfs_trans_handle *trans,
 	    !btrfs_header_flag(buf, BTRFS_HEADER_FLAG_WRITTEN) &&
 	    !(root->root_key.objectid != BTRFS_TREE_RELOC_OBJECTID &&
 	      btrfs_header_flag(buf, BTRFS_HEADER_FLAG_RELOC)) &&
-	    !test_bit(BTRFS_ROOT_FORCE_COW, &root->state))
+	    !test_bit(BTRFS_ROOT_FORCE_COW, &root->state) &&
+	    !should_cow_block_v2(root, buf))
 		return 0;
 	return 1;
 }
