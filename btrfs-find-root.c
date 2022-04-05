c2491acd108737504c78041de591fe327a68a68d
Josef Bacik <josef@toxicpanda.com>
2022-04-05 14:35:49 -0400

    try harder

--- btrfs-find-root.c
+++ btrfs-find-root.c
@@ -451,7 +451,9 @@ int main(int argc, char **argv)
 		return 1;
 
 	ocf.filename = argv[optind];
-	ocf.flags = OPEN_CTREE_CHUNK_ROOT_ONLY | OPEN_CTREE_IGNORE_CHUNK_TREE_ERROR;
+	ocf.flags = OPEN_CTREE_PARTIAL | OPEN_CTREE_NO_BLOCK_GROUPS |
+		OPEN_CTREE_ALLOW_TRANSID_MISMATCH | __OPEN_CTREE_RETURN_CHUNK_ROOT;
+//	ocf.flags = OPEN_CTREE_CHUNK_ROOT_ONLY | OPEN_CTREE_IGNORE_CHUNK_TREE_ERROR;
 	fs_info = open_ctree_fs_info(&ocf);
 	if (!fs_info) {
 		error("open ctree failed");
