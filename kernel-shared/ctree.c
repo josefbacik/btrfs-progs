a58d34b4198fc9addbaa038741d69887175e05eb
Josef Bacik <josef@toxicpanda.com>
2023-10-12 08:59:16 -0400

    btrfs-progs: sync btrfs_search_slot_for_read from the kernel
    This appears to just be a whitespace issue, the code remains largely
    unchanged.
    
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>

--- kernel-shared/ctree.c
+++ kernel-shared/ctree.c
@@ -2421,7 +2421,7 @@ static int btrfs_prev_leaf(struct btrfs_root *root, struct btrfs_path *path)
 }
 
 /*
- * Helper to use instead of search slot if no exact match is needed but
+ * helper to use instead of search slot if no exact match is needed but
  * instead the next or previous item should be returned.
  * When find_higher is true, the next higher item is returned, the next lower
  * otherwise.
@@ -2433,65 +2433,68 @@ static int btrfs_prev_leaf(struct btrfs_root *root, struct btrfs_path *path)
  * < 0 on error
  */
 int btrfs_search_slot_for_read(struct btrfs_root *root,
-                               const struct btrfs_key *key,
-                               struct btrfs_path *p, int find_higher,
-                               int return_any)
+			       const struct btrfs_key *key,
+			       struct btrfs_path *p, int find_higher,
+			       int return_any)
 {
-        int ret;
-        struct extent_buffer *leaf;
+	int ret;
+	struct extent_buffer *leaf;
 
 again:
-        ret = btrfs_search_slot(NULL, root, key, p, 0, 0);
-        if (ret <= 0)
-                return ret;
-        /*
-	 * A return value of 1 means the path is at the position where the item
-	 * should be inserted. Normally this is the next bigger item, but in
-	 * case the previous item is the last in a leaf, path points to the
-	 * first free slot in the previous leaf, i.e. at an invalid item.
-         */
-        leaf = p->nodes[0];
+	ret = btrfs_search_slot(NULL, root, key, p, 0, 0);
+	if (ret <= 0)
+		return ret;
+	/*
+	 * a return value of 1 means the path is at the position where the
+	 * item should be inserted. Normally this is the next bigger item,
+	 * but in case the previous item is the last in a leaf, path points
+	 * to the first free slot in the previous leaf, i.e. at an invalid
+	 * item.
+	 */
+	leaf = p->nodes[0];
 
-        if (find_higher) {
-                if (p->slots[0] >= btrfs_header_nritems(leaf)) {
-                        ret = btrfs_next_leaf(root, p);
-                        if (ret <= 0)
-                                return ret;
-                        if (!return_any)
-                                return 1;
-                        /*
-			 * No higher item found, return the next lower instead
-                         */
-                        return_any = 0;
-                        find_higher = 0;
-                        btrfs_release_path(p);
-                        goto again;
-                }
-        } else {
-                if (p->slots[0] == 0) {
-                        ret = btrfs_prev_leaf(root, p);
-                        if (ret < 0)
-                                return ret;
-                        if (!ret) {
-                                leaf = p->nodes[0];
-                                if (p->slots[0] == btrfs_header_nritems(leaf))
-                                        p->slots[0]--;
-                                return 0;
-                        }
-                        if (!return_any)
-                                return 1;
-                        /*
-			 * No lower item found, return the next higher instead
-                         */
-                        return_any = 0;
-                        find_higher = 1;
-                        btrfs_release_path(p);
-                        goto again;
-                } else {
-                        --p->slots[0];
-                }
-        }
-        return 0;
+	if (find_higher) {
+		if (p->slots[0] >= btrfs_header_nritems(leaf)) {
+			ret = btrfs_next_leaf(root, p);
+			if (ret <= 0)
+				return ret;
+			if (!return_any)
+				return 1;
+			/*
+			 * no higher item found, return the next
+			 * lower instead
+			 */
+			return_any = 0;
+			find_higher = 0;
+			btrfs_release_path(p);
+			goto again;
+		}
+	} else {
+		if (p->slots[0] == 0) {
+			ret = btrfs_prev_leaf(root, p);
+			if (ret < 0)
+				return ret;
+			if (!ret) {
+				leaf = p->nodes[0];
+				if (p->slots[0] == btrfs_header_nritems(leaf))
+					p->slots[0]--;
+				return 0;
+			}
+			if (!return_any)
+				return 1;
+			/*
+			 * no lower item found, return the next
+			 * higher instead
+			 */
+			return_any = 0;
+			find_higher = 1;
+			btrfs_release_path(p);
+			goto again;
+		} else {
+			--p->slots[0];
+		}
+	}
+	return 0;
 }
 
 /*
